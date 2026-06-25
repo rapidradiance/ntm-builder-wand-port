a697e1465307c0996f837c3720acfcada83b1c5c
codiak540 <tomo42806@gmail.com>
2026-06-25 00:08:17 -0400

    fixeed

--- src/main/java/net/radiance/ntmbwp/Ntmbwp.java
+++ src/main/java/net/radiance/ntmbwp/Ntmbwp.java
@@ -3,21 +3,18 @@ package net.radiance.ntmbwp;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.radiance.ntmbwp.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 @Mod(Ntmbwp.MOD_ID)
-@EventBusSubscriber
 public class Ntmbwp {
 
     public static final String MOD_ID = "ntmbwp";
@@ -27,14 +24,22 @@ public class Ntmbwp {
         container.registerConfig(ModConfig.Type.COMMON, net.radiance.ntmbwp.CommonConfig.SPEC, "ntmbwp-common.toml");
         ModDataComponents.register(modEventBus);
         ModItems.register(container.getEventBus());
+
+
     }
 
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public static void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(Minecraft.getInstance().options.operatorItemsTab().get()){
-            if(event.getTabKey() == CreativeModeTabs.OP_BLOCKS) {
-                event.accept(ModItems.WAND);
+    @EventBusSubscriber
+    @Mod(value = Ntmbwp.MOD_ID, dist = Dist.CLIENT)
+    public static class NtmbwpClient {
+        public NtmbwpClient(IEventBus modBus) {
+        }
+
+        @SubscribeEvent
+        public static void addCreative(BuildCreativeModeTabContentsEvent event) {
+            if (Minecraft.getInstance().options.operatorItemsTab().get()) {
+                if (event.getTabKey() == CreativeModeTabs.OP_BLOCKS) {
+                    event.accept(ModItems.WAND);
+                }
             }
         }
     }
